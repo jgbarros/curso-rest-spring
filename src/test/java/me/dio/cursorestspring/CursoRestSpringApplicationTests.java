80ca4aecc90f1cb50bd332f304f776d0f0e9e399
João Guilherme Barros <jgbarros@gmail.com>
2024-02-17 11:36:23 -0400

    Initial commit

---
+++ src/test/java/me/dio/cursorestspring/CursoRestSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.cursorestspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoRestSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
